a7fdcff010d9e3c54caf60b6a7a24dc68d0ad1ac
Sebastian Borgeaud <sebastian.borgeaud@gmail.com>
2016-12-24 10:57:44 +0100

    Rotate images for data augmentation

--- cnn.py
+++ cnn.py
@@ -23,9 +23,7 @@ args = parser.parse_args()
 samples = 183396 * 5
 
 dataExtractor = DataExtractor(images, labels, image_dimensions)
-
 X, y = dataExtractor.extractTrainingData(numSamples = samples)
-
 print("Input data shape", X.shape, y.shape)
 
 #add if statement checking if weigths for the model have been saved
@@ -37,27 +35,43 @@ else:
     model_file = args.model
     print("Loading model from", model_file)
     model = load_model(model_file)
-
+    
 batch_size = 32
 nb_epoch = 1
 verbose = 1
+rotateImages = True
 
-model.fit(
+def rotate(k, Xs):
+    if k == 0:
+        return Xs
+    else:
+        rotated = [np.rot90(X) for X in Xs]
+        return np.array(rotated)
+
+#rotate all images by all 4 possible 90 deg angles (0, 90, 180, 270)
+def dataGenerator():
+    datagen = ImageDataGenerator()
+    for Xs, ys in datagen.flow(X, y, batch_size=batch_size):
+        print(Xs.shape, ys.shape)
+        for i in range(4):
+            yield rotate(i, Xs), ys
+
+if rotateImages:
+    model.fit_generator(
+        dataGenerator(),
+        samples_per_epoch = X.shape[0] * 4,
+        nb_epoch = nb_epoch, 
+        verbose = verbose
+        )
+else:
+    model.fit(
         X, 
         y,
         batch_size = 32,
         nb_epoch = nb_epoch,
-        verbose = verbose,
-        validation_split = 0.1)
+        verbose = verbose
+        )
 
-#datagen = ImageDataGenerator(rotation_range=20)
-#
-#model.fit_generator(
-#        datagen.flow(X, y, batch_size=batch_size),
-#        samples_per_epoch = X.shape[0],
-#        nb_epoch = nb_epoch, 
-#        verbose = verbose, 
-#        callbacks = [checkpointer])
 
 
 #Save the model
@@ -65,4 +79,4 @@ timestamp = time.strftime('%Y-%m-%d_%H:%M:%S')
 filePath = args.save_dir + timestamp + ".h5"
 model.save(filePath)
 print("Saved the model to", filePath)
-model.save('./models/latest.h5')
+model.save(args.save_dir + 'latest.h5')

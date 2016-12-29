e7a549334c4a8080c026a63add73bcad859dc031
Sebastian Borgeaud <sebastian.borgeaud@gmail.com>
2016-12-29 21:40:11 +0100

    Refactor input parsing to different file

--- cnn.py
+++ cnn.py
@@ -2,37 +2,34 @@ from loadImagesFromDisk import loadImages
 from dataExtractor import DataExtractor
 from visualise import showSlice
 from model import createModel
+from inputParser import parse_input
 
 import numpy as np
-
 import time
-import argparse
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
 
-parser = argparse.ArgumentParser(description="BRATS CNN")
-parser.add_argument('data_dir', metavar="data directory", type=str, help="The directory in which the training data is saved")
-parser.add_argument('save_dir', metavar="model destination directory", type=str, help="The directory in which the model will be saved after training")
-parser.add_argument('--model', metavar="model file", type=str, nargs='?', help="The hd5 file of the model to load")
+args = parse_input()
+data_dir = args.data_dir
+model_file = args.model
+save_dir = args.save_dir
 
-args = parser.parse_args()
-(images, labels, image_dimensions) = loadImages(args.data_dir)
+(images, labels, image_dimensions) = loadImages(data_dir)
 
 #the smallest class consists of 183396 patches, thus we have 5 times that many samples in total
-samples = 183396 * 5
+samples = 1000 #183396 * 5
 
 dataExtractor = DataExtractor(images, labels, image_dimensions)
 X, y = dataExtractor.extractTrainingData(numSamples = samples)
 print("Input data shape", X.shape, y.shape)
 
 #add if statement checking if weigths for the model have been saved
-if args.model == None:
+if model_file == None:
     print("Creating a new model")
     model = createModel(X[0].shape)
     model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
 else:
-    model_file = args.model
     print("Loading model from", model_file)
     model = load_model(model_file)
     
@@ -52,10 +49,10 @@ def rotate(k, Xs):
 def dataGenerator():
     datagen = ImageDataGenerator()
     for Xs, ys in datagen.flow(X, y, batch_size=batch_size):
-        print(Xs.shape, ys.shape)
         for i in range(4):
             yield rotate(i, Xs), ys
 
+#train the model
 if rotateImages:
     model.fit_generator(
         dataGenerator(),
@@ -76,7 +73,7 @@ else:
 
 #Save the model
 timestamp = time.strftime('%Y-%m-%d_%H:%M:%S')
-filePath = args.save_dir + timestamp + ".h5"
+filePath = save_dir + timestamp + ".h5"
 model.save(filePath)
 print("Saved the model to", filePath)
-model.save(args.save_dir + 'latest.h5')
+model.save(save_dir + 'latest.h5')
---
+++ inputParser.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="BRATS CNN")
+parser.add_argument('data_dir', metavar="data directory", type=str, help="The directory in which the training data is saved")
+parser.add_argument('save_dir', metavar="model destination directory", type=str, help="The directory in which the model will be saved after training")
+parser.add_argument('--model', metavar="model file", type=str, nargs='?', help="The hd5 file of the model to load")
+
+def parse_input():
+    args = parser.parse_args()
+    print(args)
+    return args 

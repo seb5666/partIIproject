16ffcdd5d45aa98faf4c2fb90ff4f72bc086cda8
Sebastian Borgeaud <sebastian.borgeaud@gmail.com>
2017-01-24 17:44:37 +0000

    Change activation to Leaky ReLu

--- model.py
+++ model.py
@@ -2,37 +2,40 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Convolution2D, MaxPooling2D, Dropout
 
 def createModel(input_shape):
+    
+    LReLu = keras.layers.advanced_activations.LeakyReLu(alpha=0.333)
+
     model = Sequential()
 
     model.add(Convolution2D(64, 3, 3, border_mode='same', input_shape = input_shape))
-    model.add(Activation('relu'))
+    model.add(LReLu)
 
     model.add(Convolution2D(64, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
+    model.add(LReLu)
 
     model.add(Convolution2D(64, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
+    model.add(LReLu)
 
     model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2), border_mode='valid'))
 
     model.add(Convolution2D(128, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
+    model.add(LReLu)
 
     model.add(Convolution2D(128, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
+    model.add(LReLu)
 
     model.add(Convolution2D(128, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
+    model.add(LReLu)
 
     model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2), border_mode='valid'))
     model.add(Flatten())
 
     model.add(Dense(256))
-    model.add(Activation('relu'))
+    model.add(LReLu)
     model.add(Dropout(0.1))
 
     model.add(Dense(256))
-    model.add(Activation('relu'))
+    model.add(LReLu)
     model.add(Dropout(0.1))
 
     model.add(Dense(5))

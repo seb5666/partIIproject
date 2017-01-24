963a2c4b18ea62a07e5093358cb74225caec0783
Sebastian Borgeaud <sebastian.borgeaud@gmail.com>
2017-01-24 17:43:32 +0000

    Refactor image loading procedure

--- loadImagesFromDisk.py
+++ loadImagesFromDisk.py
@@ -7,20 +7,27 @@ import os
 from os import listdir
 
 #Load all the training images and the labels that are within a high level directory (/HG/ for ex.)
-def loadImages(images_directory):
+def loadImages(images_directory, use_N4Correction = True):
     images = []
     labels = []
     dimensions = []
     for data_dir in sorted(listdir(images_directory)):
         if not(data_dir.startswith('.')):
-            (image, label, dimension) = loadImage(os.path.join(images_directory, data_dir))
+            OT = loadGroundTruth(os.path.join(images_directory, data_dir))
+            scans = loadScans(os.path.join(images_directory, data_dir), use_N4Correction)
+            image = np.array([sitk.GetArrayFromImage(scan) for scan in scans]).astype('float32')
+            image, dimension = stackImage(image)
+            label = sitk.GetArrayFromImage(OT)
+            
+            print("Read in an image of size", image.shape, "from", images_directory)
+
             images.append(image)
             labels.append(label)
             dimensions.append(dimension)
+
     return (images, labels, dimensions)
 
-def loadImage(image_directory, use_N4Correction = True):
-    ot = ""
+def loadScans(image_directory, use_N4Correction = True):
     t1 = ""
     t1c = ""
     t2 = ""
@@ -29,9 +36,7 @@ def loadImage(image_directory, use_N4Correction = True):
         for file in filenames:
             if file.endswith('.mha'):
                 filePath = os.path.join(dirpath, file)
-                if "OT" in file:
-                    ot = filePath
-                elif "T1c" in file:
+                if "T1c" in file:
                     if use_N4Correction and "_normalized" in file:
                         t1c = filePath
                     if not(use_N4Correction) and "_normalized" not in file:
@@ -47,44 +52,43 @@ def loadImage(image_directory, use_N4Correction = True):
                     flair = filePath
 
     imagePaths = [t1, t1c, t2, flair] 
+    images = [sitk.ReadImage(imagePath) for imagePath in imagePaths]
 
-    images = np.array([sitk.GetArrayFromImage(sitk.ReadImage(imagePath)) for imagePath in imagePaths]).astype('float32')
-    
-    if len(images) != 4 or ot == "":
+    if len(images) != 4:
         raise Exception("An error occured while reading from", image_directory)
 
     print("Reading images", imagePaths)
-    print("Reading ground truth", ot)
-    print("Read in an image of size", images.shape, "from", image_directory)
 
-    OT = sitk.ReadImage(ot)
-    labels = sitk.GetArrayFromImage(OT)
+    return images
 
-    #stack images along the 4th dimension so that image[z][y][x] containes the 4 values for the different scan types
-    data = np.stack(images, axis=3)
-    dimension = data.shape
-    return (data, labels, dimension)
-
-#Loads the 4 images but not the Observer Truth
-def loadTestImage(image_directory):
-    imagePaths = []
+def loadGroundTruth(image_directory):
+    ot = ""
     for (dirpath, dirnames, filenames) in os.walk(image_directory):
         for file in filenames:
             if file.endswith('.mha'):
                 filePath = os.path.join(dirpath, file)
-                if "OT" not in file:
-                    imagePaths.append(filePath)
+                if "OT" in file:
+                    ot = filePath
 
-    images = np.array([sitk.GetArrayFromImage(sitk.ReadImage(imagePath)) for imagePath in imagePaths]).astype('float32')
+    if ot == "":
+        raise Exception("No ground truth image was found in", image_directory)
 
-    if len(images) != 4:
-        raise Exception("An error occured while reading from", image_directory)
-    print("Reading images", imagePaths)
+    OT = sitk.ReadImage(ot)
+    print("Reading ground truth", ot)
+    return OT
 
     #stack images along the 4th dimension so that image[z][y][x] containes the 4 values for the different scan types
+def stackImage(images):
     data = np.stack(images, axis=3)
     dimension = data.shape
     return (data, dimension)
+
+#Loads the 4 images but not the Observer Truth
+def loadTestImage(image_directory, use_N4Correction = True):
+    scans = loadScans(image_directory, use_N4Correction)
+    image = np.array([sitk.GetArrayFromImage(scan) for scan in scans]).astype('float32')
+    images, dimension = stackImage(image)
+    return (images, dimension)
 
 def showImage(image, sliceNumber=0):
     plt.imshow(image[sliceNumber,: ,:])
@@ -92,7 +96,7 @@ def showImage(image, sliceNumber=0):
 
 # Load all images
 # dirPath = sys.argv[1]
-# (data, labels) = loadImages(dirPath)
+
 
 #print("Labels", labels.shape)
 #print("Data", data.shape)

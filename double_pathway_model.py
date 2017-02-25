47352806104c62437fb1490d56dd1abb5374212b
Sebastian Borgeaud <spb61@cl.cam.ac.uk>
2017-02-25 13:46:58 +0000

    Remove BN from double path and use SGD instead of Adam

--- double_pathway_model.py
+++ double_pathway_model.py
@@ -4,6 +4,7 @@ from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 from keras.regularizers import l2, l1l2
 from keras.engine.topology import Merge
+from keras.optimizers import SGD, Adam
 
 from metrics import per_class_precision, dice
 
@@ -13,23 +14,28 @@ def createModel(input_shape, tf_ordering=True, second_phase = False):
     axis = -1
     if not(tf_ordering):
         axis = 1
-
+    alpha = 0.1
     w_reg = 0.0001
+    
+    print("Hyperparameters: alpha=%f, w_reg=%f"%(alpha, w_reg))
     
     path1 = Sequential()
     path1.add(Convolution2D(64, 7, 7, border_mode='valid', input_shape = input_shape, W_regularizer=l1l2(l1 = w_reg, l2 = w_reg)))
-    path1.add(BatchNormalization(axis=axis))
+    #path1.add(BatchNormalization(axis=axis))
+    path1.add(Dropout(alpha))
     path1.add(Activation('relu'))
     path1.add(MaxPooling2D(pool_size=(4,4), strides=(1,1), border_mode='valid'))
 
     path1.add(Convolution2D(64, 3, 3, border_mode='valid', W_regularizer=l1l2(l1 = w_reg, l2 = w_reg)))
-    path1.add(BatchNormalization(axis=axis))
+    #path1.add(BatchNormalization(axis=axis))
+    path1.add(Dropout(alpha))
     path1.add(Activation('relu'))
     path1.add(MaxPooling2D(pool_size=(2,2), strides=(1,1), border_mode='valid'))
    
     path2 = Sequential()
     path2.add(Convolution2D(160, 13, 13, border_mode='valid', input_shape = input_shape, W_regularizer=l1l2(l1 = w_reg, l2 = w_reg)))
-    path2.add(BatchNormalization(axis=axis))
+    #path2.add(BatchNormalization(axis=axis))
+    path2.add(Dropout(alpha))
     path2.add(Activation('relu'))
     
     merge_layer = Sequential()
@@ -41,11 +47,14 @@ def createModel(input_shape, tf_ordering=True, second_phase = False):
     classification_layer = Sequential()
     classification_layer.add(merge_layer)
     classification_layer.add(Convolution2D(5, 21, 21, border_mode='valid', W_regularizer=l1l2(l1 = w_reg, l2 = w_reg)))
-    classification_layer.add(BatchNormalization(axis=axis))
+    #classification_layer.add(BatchNormalization(axis=axis))
+    classification_layer.add(Dropout(alpha))
     classification_layer.add(Flatten())
     classification_layer.add(Activation('softmax'))
-   
-    classification_layer.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', dice])
+    
+    sgd = SGD(lr=0.005, decay = 1e-1, momentum=0.5, nesterov=True)
+    adam = Adam(lr=0.0001)
+    classification_layer.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy', dice])
     
     path1.summary()
     path2.summary()

48feda825f91e3c1f6cb8d8fd77b32e9addc2023
Sebastian Borgeaud <sebastian.borgeaud@gmail.com>
2017-03-16 15:42:57 +0000

    Refactor double pathway arch code

--- double_pathway_model.py
+++ double_pathway_model.py
@@ -21,30 +21,23 @@ def createModel(input_shape, tf_ordering=True, second_phase = False):
     
     path1 = Sequential()
     path1.add(Convolution2D(64, 7, 7, border_mode='valid', input_shape = input_shape, W_regularizer=l1l2(l1 = w_reg, l2 = w_reg), trainable=not(second_phase)))
-    #path1.add(BatchNormalization(axis=axis))
     path1.add(Dropout(alpha))
     path1.add(Activation('relu'))
     path1.add(MaxPooling2D(pool_size=(4,4), strides=(1,1), border_mode='valid'))
 
     path1.add(Convolution2D(64, 3, 3, border_mode='valid', W_regularizer=l1l2(l1 = w_reg, l2 = w_reg), trainable=not(second_phase)))
-    #path1.add(BatchNormalization(axis=axis))
     path1.add(Dropout(alpha))
     path1.add(Activation('relu'))
     path1.add(MaxPooling2D(pool_size=(2,2), strides=(1,1), border_mode='valid'))
    
     path2 = Sequential()
     path2.add(Convolution2D(160, 13, 13, border_mode='valid', input_shape = input_shape, W_regularizer=l1l2(l1 = w_reg, l2 = w_reg), trainable=not(second_phase)))
-    #path2.add(BatchNormalization(axis=axis))
     path2.add(Dropout(alpha))
     path2.add(Activation('relu'))
     
-    merge_layer = Sequential()
-    merge_layer.add(Merge([path1, path2], mode='concat', concat_axis=axis))
-
     classification_layer = Sequential()
-    classification_layer.add(merge_layer)
+    classification_layer.add(Merge([path1, path2], mode='concat', concat_axis=axis))
     classification_layer.add(Convolution2D(5, 21, 21, border_mode='valid', W_regularizer=l1l2(l1 = w_reg, l2 = w_reg)))
-    #classification_layer.add(BatchNormalization(axis=axis))
     classification_layer.add(Dropout(alpha))
     classification_layer.add(Flatten())
     classification_layer.add(Activation('softmax'))
@@ -53,8 +46,6 @@ def createModel(input_shape, tf_ordering=True, second_phase = False):
     adam = Adam(lr=0.0005)
     classification_layer.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy', dice])
     
-    path1.summary()
-    path2.summary()
     classification_layer.summary()
     
     return classification_layer

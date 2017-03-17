01d2f3ee0a6409ccfa717a05aecd4184397fb504
Sebastian Borgeaud <sebastian.borgeaud@gmail.com>
2017-03-17 12:08:56 +0000

    Add script to parse results from CSV

---
+++ parse_results.py
@@ -0,0 +1,70 @@
+#script to parse results from the online platform
+
+import sys
+import csv
+import re
+
+assert(len(sys.argv) == 2)
+
+with open(sys.argv[1], newline='') as csvfile:
+    results = list(csv.reader(csvfile, delimiter=';'))
+
+headers = results[4]
+
+results = [r for r in results if len(r) > 1 and r[0] == 'Sebastian']
+
+submissions = {}
+
+#sort results by submission
+pattern = 'sub(\d+)'
+for row in results:
+    matches = re.findall(pattern, row[headers.index('Name')])
+    if len(matches) != 1:
+        raise Exception("Error while processing the CSV. The following row didn't match for a single sub number", row)
+    sub_number = int(matches[0]) 
+    if sub_number in submissions:
+        submissions[sub_number].append(row)
+    else:
+        submissions[sub_number] = [row]
+
+
+#sort regions by scan
+scan_pattern = '03(\d{2})'
+for k in submissions:
+    scans = {}
+    for i in range(1,11):
+        scans[i] = []
+    for row in submissions[k]:
+        matches = re.findall(scan_pattern, row[headers.index('Name')])
+        if len(matches) != 1:
+            raise Exception("Error while processing the CSV. Row doesn't contain a submission number", row)
+        scan_number = int(matches[0])
+        scans[scan_number].append(row)
+    if len(scans) != 10:
+        print("Submission {} doesn't have 10 scans".format(k))
+    else:
+        submissions[k] = scans
+
+#sort submissions by region
+region_column = headers.index('Region')
+
+for k in submissions:
+    for scan in submissions[k]:
+        if len(submissions[k][scan]) != 3:
+            print("Submission {} contains duplicates for scan {}".format(k, scan))
+        regions = {}
+        for row in submissions[k][scan]:
+            region = int(row[region_column])
+            regions[region] = row
+        submissions[k][scan] = regions
+
+dice = {}
+#compute average dice scores for submissions
+for sub in submissions:
+    dice_scores = {1:0, 2:0, 3:0}
+    for scan in submissions[sub]:
+        for region in submissions[sub][scan]:
+            dice_scores[region] += float(submissions[sub][scan][region][headers.index('Dice')]) / 10
+    dice[sub] = dice_scores
+for sub in dice:
+    print("Sub {:2}, {:0.3f} {:0.3f} {:0.3f}".format(sub, dice[sub][1], dice[sub][2], dice[sub][3]))

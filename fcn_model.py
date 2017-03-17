34562fb45b3954e702d872b3e2066cff01d16c18
Sebastian Borgeaud <sebastian.borgeaud@gmail.com>
2017-03-17 10:58:02 +0000

    Refactor FCN model to use functional api

--- fcn_model.py
+++ fcn_model.py
@@ -1,5 +1,5 @@
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Convolution2D, MaxPooling2D, Dropout, Permute, Reshape
+from keras.models import Model
+from keras.layers import Dense, Activation, Flatten, Convolution2D, MaxPooling2D, Dropout, Permute, Reshape, Input
 from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 from keras.regularizers import l2
@@ -16,60 +16,72 @@ def createModel(input_shape, tf_ordering=True, second_training_phase = False):
 
     l = 0
     init = 'glorot_normal'
-    p = 0.1
+    p = 0.0
     trainable = True
     print("Creating new fully convolutional model with input shape", input_shape)
     print("Parameters l=%f, alpha=%f, init=%s, p=%f"%(l, alpha, init, p))
-
-    model = Sequential()
-
-    model.add(Convolution2D(64, 3, 3, border_mode='same', init=init, input_shape = input_shape, W_regularizer=l2(l), trainable=trainable))
-    model.add(BatchNormalization(axis=axis, trainable=trainable))
-    model.add(Dropout(p))
-    model.add(LeakyReLU(alpha))
     
-    model.add(Convolution2D(64, 3, 3, border_mode='same', init=init, W_regularizer=l2(l), trainable=trainable))
-    model.add(BatchNormalization(axis=axis, trainable=trainable))
-    model.add(Dropout(p))
-    model.add(LeakyReLU(alpha))
+    inputs = Input(shape=input_shape)
 
+    h1 = Convolution2D(64, 3, 3, border_mode='same', init=init, input_shape=input_shape, W_regularizer=l2(l))(inputs)
+    h1 = BatchNormalization(axis=axis)(h1)
+    h1 = Dropout(p)(h1)
+    h1 = Activation('relu')(h1)
 
-    model.add(MaxPooling2D(pool_size=(2,2), border_mode='valid', trainable=trainable))
+    h2 = Convolution2D(64, 3, 3, border_mode='same', init=init, W_regularizer=l2(l))(h1) 
+    h2 = BatchNormalization(axis=axis)(h2)
+    h2 = Dropout(p)(h2)
+    h2 = Activation('relu')(h2)
 
-    model.add(Convolution2D(128, 3, 3, border_mode='same', init=init, W_regularizer=l2(l), trainable=trainable))
-    model.add(BatchNormalization(axis=axis, trainable=trainable))
-    model.add(Dropout(p))
-    model.add(LeakyReLU(alpha))
+    h3 = MaxPooling2D(pool_size=(2,2), border_mode='valid')(h2)
     
-    model.add(Convolution2D(128, 3, 3, border_mode='same', init=init, W_regularizer=l2(l), trainable=trainable))
-    model.add(BatchNormalization(axis=axis, trainable=trainable))
-    model.add(Dropout(p))
-    model.add(LeakyReLU(alpha))
-
-    model.add(Convolution2D(128, 3, 3, border_mode='same', init=init, W_regularizer=l2(l), trainable=trainable))
-    model.add(BatchNormalization(axis=axis, trainable=trainable))
-    model.add(Dropout(p))
-    model.add(LeakyReLU(alpha))
-
-    model.add(MaxPooling2D(pool_size=(2,2), border_mode='valid', trainable=trainable))
-
-    model.add(Convolution2D(256, 3, 3, border_mode='same', init=init, W_regularizer=l2(l), trainable=trainable))
-    model.add(BatchNormalization(axis=axis, trainable=trainable))
-    model.add(Dropout(p))
-    model.add(LeakyReLU(alpha))
+    h4 = Convolution2D(128, 3, 3, border_mode='same', init=init, W_regularizer=l2(l))(h3)
+    h4 = BatchNormalization(axis=axis)(h4)
+    h4 = Dropout(p)(h4)
+    h4 = Activation('relu')(h4)
     
-    model.add(Convolution2D(256, 3, 3, border_mode='same', init=init, W_regularizer=l2(l), trainable=trainable))
-    model.add(BatchNormalization(axis=axis, trainable=trainable))
-    model.add(Dropout(p))
-    model.add(LeakyReLU(alpha))
-    
-    model.add(MaxPooling2D(pool_size=(2,2), border_mode='valid', trainable=trainable))
+    h5 = Convolution2D(128, 3, 3, border_mode='same', init=init, W_regularizer=l2(l))(h4)
+    h5 = BatchNormalization(axis=axis)(h5)
+    h5 = Dropout(p)(h5)
+    h5 = Activation('relu')(h5)
 
-    model.add(Convolution2D(5, 3, 3, border_mode='same', init=init, W_regularizer=l2(l), trainable=trainable))
+    h6 = Convolution2D(128, 3, 3, border_mode='same', init=init, W_regularizer=l2(l))(h5)
+    h6 = BatchNormalization(axis=axis)(h6)
+    h6 = Dropout(p)(h6)
+    h6 = Activation('relu')(h6)
+
+    h7 = MaxPooling2D(pool_size=(2,2), border_mode='valid')(h6)
+
+    h8 = Convolution2D(256, 3, 3, border_mode='same', init=init, W_regularizer=l2(l))(h7)
+    h8 = BatchNormalization(axis=axis)(h8)
+    h8 = Dropout(p)(h8)
+    h8 = Activation('relu')(h8)
     
-    model.add(Reshape((5, 64)))
-    model.add(Permute((2,1)))
-    model.add(Activation('softmax'))
+    h9 = Convolution2D(256, 3, 3, border_mode='same', init=init, W_regularizer=l2(l))(h8)
+    h9 = BatchNormalization(axis=axis)(h9)
+    h9 = Dropout(p)(h9)
+    h9 = Activation('relu')(h9)
+
+    h10 = MaxPooling2D(pool_size=(2,2), border_mode='valid')(h9)
+
+    h11 = Convolution2D(256, 3, 3, border_mode='same', init=init, W_regularizer=l2(l))(h10)
+    h11 = BatchNormalization(axis=axis)(h11)
+    h11 = Dropout(p)(h11)
+    h11 = Activation('relu')(h11)
+
+    h12 = Convolution2D(256, 3, 3, border_mode='same', init=init, W_regularizer=l2(l))(h11)
+    h12 = BatchNormalization(axis=axis)(h12)
+    h12 = Dropout(p)(h12)
+    h12 = Activation('relu')(h12)
+    
+    h13 = Convolution2D(5, 3, 3, border_mode='same', init=init, W_regularizer=l2(l))(h12)
+    
+    predictions = Reshape((5, 64))(h13)
+    predictions = Permute((2,1))(predictions)
+    predictions = Activation('softmax')(predictions)
+    
+    model = Model(input=[inputs], output=predictions)
+
     sgd = SGD(lr = 3e-5, decay =0.0, momentum = 0.9, nesterov = True)
     adam = Adam()
     model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
